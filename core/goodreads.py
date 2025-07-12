eaf217f7d96dd8fcd682b54b9eb40a89a157a6f9
j99carmona <j99.carmona@gmail.com>
2025-07-12 19:24:02 +0200

    feat: load_file_columns for csv

--- core/goodreads.py
+++ core/goodreads.py
@@ -95,8 +95,10 @@ class GoodReadsData:
 
     def load_file_columns(self, filename: str, columns: list[str]
                           ) -> pd.DataFrame:
+        
+        if self._file_names.loc[filename, "ext"] == ".csv":
+            return pd.read_csv(self.get_file_path(filename), usecols=columns)
         data = []
-        # df = pd.read_csv("dataset.csv", usecols=["col1", "col2"])
         with gzip.open(self.get_file_path(filename), "rt", encoding="utf-8") as fin:
             for l in fin:
                 obj = json.loads(l)
